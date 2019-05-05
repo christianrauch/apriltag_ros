c5a2737baac22f456593d71dfdf15747e615dcb5
Christian Rauch <Rauch.Christian@gmx.de>
2019-05-05 18:00:11 +0100

    remove standalone node

--- node/src/apriltag_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <AprilTagNode.hpp>
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<AprilTagNode>());
-    rclcpp::shutdown();
-    return 0;
-}
