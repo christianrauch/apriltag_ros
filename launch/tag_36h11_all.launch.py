231f2a0f8c3bff0b2424c9ff5b00f7ef85344ea8
Christian Rauch <Rauch.Christian@gmx.de>
2022-08-27 22:11:04 +0200

    composable node example launch file with 'usb_cam' node

--- launch/tag_36h11_all.launch.py
+++ launch/tag_36h11_all.launch.py
@@ -12,17 +12,31 @@ cfg_36h11 = {
 }
 
 def generate_launch_description():
-    composable_node = ComposableNode(
-        name='apriltag',
+    # 'usb_cam' node from https://github.com/ros-drivers/usb_cam.git
+    cam_node = ComposableNode(
+        namespace='camera',
+        package='usb_cam', plugin='usb_cam::UsbCamNode',
+        extra_arguments=[{'use_intra_process_comms': True}],
+    )
+    tag_node = ComposableNode(
+        name='apriltag_36h11',
+        namespace='apriltag',
         package='apriltag_ros', plugin='AprilTagNode',
-        remappings=[("/apriltag/image_rect", "/camera/image"), ("/apriltag/camera_info", "/camera/camera_info")],
-        parameters=[cfg_36h11])
+        remappings=[
+            # This maps the 'raw' images for simplicity of demonstration.
+            # In practice, this will have to be the rectified 'rect' images.
+            ("/apriltag/image_rect", "/camera/image_raw"),
+            ("/apriltag/camera_info", "/camera/camera_info"),
+        ],
+        parameters=[cfg_36h11],
+        extra_arguments=[{'use_intra_process_comms': True}],
+    )
     container = ComposableNodeContainer(
         name='tag_container',
         namespace='apriltag',
         package='rclcpp_components',
         executable='component_container',
-        composable_node_descriptions=[composable_node],
+        composable_node_descriptions=[cam_node, tag_node],
         output='screen'
     )
 
