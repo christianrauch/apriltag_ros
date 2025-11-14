0236c88f3a0a53672cfdc56c222b4209971a9dba
cherie <cheriehuziying@gmail.com>
2025-11-14 15:54:58 +0800

    multiple tag bundle

--- src/pose_estimation.hpp
+++ src/pose_estimation.hpp
@@ -3,9 +3,16 @@
 #include <apriltag/apriltag.h>
 #include <functional>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include <opencv2/calib3d.hpp>
 #include <unordered_map>
 
 
 typedef std::function<geometry_msgs::msg::Transform(apriltag_detection_t* const, const std::array<double, 4>&, const double&)> pose_estimation_f;
 
 extern const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods;
+
+cv::Point3d apply_transform_to_point3d(cv::Point3d point, std::vector<double> tf_vec);
+geometry_msgs::msg::Transform pnp_bundle(std::vector<apriltag_detection_t*> detections,
+                                         const std::array<double, 4>& intr,
+                                         std::unordered_map<int, double> tagsizes,
+                                         std::unordered_map<int, std::vector<double>> transforms);
