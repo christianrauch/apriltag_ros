c0f75aae85c8762631c6357f329e25c2d4b475ff
cherie <cheriehuziying@gmail.com>
2025-11-14 15:54:58 +0800

    multiple tag bundle

--- src/pose_estimation.cpp
+++ src/pose_estimation.cpp
@@ -4,7 +4,7 @@
 #include <apriltag/common/homography.h>
 #include <opencv2/calib3d.hpp>
 #include <tf2/convert.h>
-
+#include <tf2_eigen/tf2_eigen.hpp>
 
 geometry_msgs::msg::Transform
 homography(apriltag_detection_t* const detection, const std::array<double, 4>& intr, double tagsize)
@@ -52,6 +52,78 @@ pnp(apriltag_detection_t* const detection, const std::array<double, 4>& intr, do
     cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
 
     return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+}
+
+geometry_msgs::msg::Transform
+pnp_bundle(std::vector<apriltag_detection_t*> detections,
+           const std::array<double, 4>& intr,
+           std::unordered_map<int, double> tagsizes,
+           std::unordered_map<int, std::vector<double>> transforms)
+{
+    std::vector<cv::Point3d> objectPoints;
+    std::vector<cv::Point2d> imagePoints;
+
+    for(auto& detection : detections) {
+        int id = detection->id;
+        std::vector<cv::Point3d> untransformed_object_points = {cv::Point3d(-(tagsizes[id] / 2), -(tagsizes[id] / 2), 0),
+                                                                cv::Point3d(+(tagsizes[id] / 2), -(tagsizes[id] / 2), 0),
+                                                                cv::Point3d(+(tagsizes[id] / 2), +(tagsizes[id] / 2), 0),
+                                                                cv::Point3d(-(tagsizes[id] / 2), +(tagsizes[id] / 2), 0)};
+
+        std::vector<double> transformation_vec = transforms[detection->id];
+        // Add transformed object points to objectPoints
+        for(cv::Point3d& untransformed_obj_pt : untransformed_object_points) {
+            cv::Point3d transformed_obj_pt = apply_transform_to_point3d(
+                untransformed_obj_pt, transformation_vec);
+            objectPoints.push_back(transformed_obj_pt);
+        }
+
+        // Add image points
+        for(int i = 0; i < 4; i++) {
+            imagePoints.push_back({detection->p[i][0], detection->p[i][1]});
+        }
+    }
+
+    cv::Matx33d cameraMatrix;
+    cameraMatrix(0, 0) = intr[0];// fx
+    cameraMatrix(1, 1) = intr[1];// fy
+    cameraMatrix(0, 2) = intr[2];// cx
+    cameraMatrix(1, 2) = intr[3];// cy
+
+    cv::Mat rvec, tvec;
+    cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
+
+    return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+}
+
+cv::Point3d apply_transform_to_point3d(cv::Point3d point, std::vector<double> tf_vec)
+{
+    Eigen::Vector3d point_vec(point.x, point.y, point.z);
+
+    // Apply rotation first
+    tf2::Quaternion quat;
+    quat.setRPY(tf_vec[3], tf_vec[4], tf_vec[5]);
+    Eigen::Vector3d rotated_point_vec;
+    geometry_msgs::msg::TransformStamped rotation;
+    rotation.transform.rotation.x = quat.x();
+    rotation.transform.rotation.y = quat.y();
+    rotation.transform.rotation.z = quat.z();
+    rotation.transform.rotation.w = quat.w();
+    tf2::doTransform(point_vec, rotated_point_vec, rotation);
+
+    // Then apply a translation
+    Eigen::Vector3d translated_pt_vec;
+    geometry_msgs::msg::TransformStamped translation;
+    translation.transform.translation.x = tf_vec[0];
+    translation.transform.translation.y = tf_vec[1];
+    translation.transform.translation.z = tf_vec[2];
+    tf2::doTransform(rotated_point_vec, translated_pt_vec, translation);
+
+    cv::Point3d transformed_point;
+    transformed_point.x = translated_pt_vec[0];
+    transformed_point.y = translated_pt_vec[1];
+    transformed_point.z = translated_pt_vec[2];
+    return transformed_point;
 }
 
 const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods{
