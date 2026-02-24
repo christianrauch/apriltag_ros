0cb7c4a6473347c89d87a3ee8356e1246ef404c8
Joseph Duchesne <joseph@upsiderobotics.com>
2026-02-24 17:23:56 +0000

    Migrated camera matrix + pnp to its own method as requested
    (cherry picked from commit 433b21fe063741d6d3330daab6b02ef574608827)

--- src/pose_estimation.cpp
+++ src/pose_estimation.cpp
@@ -25,6 +25,24 @@ homography(apriltag_detection_t* const detection, const std::array<double, 4>& i
     return tf2::toMsg<apriltag_pose_t, geometry_msgs::msg::Transform>(const_cast<const apriltag_pose_t&>(pose));
 }
 
+geometry_msgs::msg::Transform pnp_from_points(
+    const std::vector<cv::Point3d>& objectPoints,
+    const std::vector<cv::Point2d>& imagePoints,
+    const std::array<double, 4>& intr
+) {
+    cv::Matx33d cameraMatrix;
+    cameraMatrix(0, 0) = intr[0];// fx
+    cameraMatrix(1, 1) = intr[1];// fy
+    cameraMatrix(0, 2) = intr[2];// cx
+    cameraMatrix(1, 2) = intr[3];// cy
+
+    cv::Mat rvec, tvec;
+    cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
+
+    return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+}
+
+
 geometry_msgs::msg::Transform
 pnp(apriltag_detection_t* const detection, const std::array<double, 4>& intr, double tagsize)
 {
@@ -42,16 +60,7 @@ pnp(apriltag_detection_t* const detection, const std::array<double, 4>& intr, do
         {detection->p[3][0], detection->p[3][1]},
     };
 
-    cv::Matx33d cameraMatrix;
-    cameraMatrix(0, 0) = intr[0];// fx
-    cameraMatrix(1, 1) = intr[1];// fy
-    cameraMatrix(0, 2) = intr[2];// cx
-    cameraMatrix(1, 2) = intr[3];// cy
-
-    cv::Mat rvec, tvec;
-    cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
-
-    return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+    return pnp_from_points(objectPoints, imagePoints, intr);
 }
 
 geometry_msgs::msg::Transform
@@ -63,11 +72,12 @@ pnp_bundle(std::vector<apriltag_detection_t*> detections,
     std::vector<cv::Point3d> objectPoints;
     std::vector<cv::Point2d> imagePoints;
 
-    for(auto& detection : detections) {
+    for(const apriltag_detection_t* detection : detections) {
         int id = detection->id;
         double s = tagsizes[id] / 2;
         std::vector<double> tf = transforms[detection->id];
 
+        // note: The tag bundles should probably store pre-computed bundle-frame tag points rather than recalculating it every detection
         std::vector<Eigen::Vector3d> corners = {
             Eigen::Vector3d(-s, -s, 0),
             Eigen::Vector3d(+s, -s, 0),
@@ -86,18 +96,8 @@ pnp_bundle(std::vector<apriltag_detection_t*> detections,
         }
     }
 
-    cv::Matx33d cameraMatrix;
-    cameraMatrix(0, 0) = intr[0];// fx
-    cameraMatrix(1, 1) = intr[1];// fy
-    cameraMatrix(0, 2) = intr[2];// cx
-    cameraMatrix(1, 2) = intr[3];// cy
-
-    cv::Mat rvec, tvec;
-    cv::solvePnP(objectPoints, imagePoints, cameraMatrix, {}, rvec, tvec);
-
-    return tf2::toMsg<std::pair<cv::Mat_<double>, cv::Mat_<double>>, geometry_msgs::msg::Transform>(std::make_pair(tvec, rvec));
+    return pnp_from_points(objectPoints, imagePoints, intr);
 }
-
 const std::unordered_map<std::string, pose_estimation_f> pose_estimation_methods{
     {"homography", homography},
     {"pnp", pnp},
